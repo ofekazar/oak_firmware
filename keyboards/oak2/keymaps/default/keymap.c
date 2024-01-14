696cd1229ccf7e48a23787d34e5b36e23162c216
ofek azarya <ofekazaray3@gmail.com>
2024-01-14 17:10:49 +0200

    a

---
+++ keyboards/oak2/keymaps/default/keymap.c
@@ -0,0 +1,23 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     /*
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *               ┌───┐           ┌───┐
+      *               │Bsp├───┐   ┌───┤Ent│
+      *               └───┤Tab│   │Spc├───┘
+      *                   └───┘   └───┘
+      */
+    [0] = LAYOUT(
+                 KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+                 KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+        KC_W,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_R,
+        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_T,    KC_D
+    )
+};
---
+++ keyboards/oak3/keymaps/default/keymap.c
@@ -0,0 +1,24 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     /*
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │Tab│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │Bsp│
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │Ctl│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │Sft│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Sft│
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
+      *               ┌───┐                   ┌───┐
+      *               │GUI├───┐           ┌───┤Alt│
+      *               └───┤Bsp├───┐   ┌───┤Ent├───┘
+      *                   └───┤   │   │   ├───┘
+      *                       └───┘   └───┘
+      */
+    [0] = LAYOUT_split_3x6_3(
+                 KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+                 KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+        KC_W,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_R,
+        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_T,    KC_D
+    )
+};
